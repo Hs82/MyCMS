9276767ed9d7b3d3ffb09e52f0c2523aa48678e3
H82s <2015shibuya@gmail.com>
2015-11-16 02:09:40 +0900

    修正

--- index.php
+++ index.php
@@ -28,8 +28,7 @@
     			$cat_id=$cats_row['cat_id'];
     			$cat_title=$cats_row['cat_title'];
 
-    			echo "
-    			<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
+    			echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
 
     		}
 
